631441f8ee2608207371826518383f186dcaaa7e
Kiran K <kiran@dub.co>
2024-10-01 20:07:52 +0530

    update response

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,16 +4,7 @@ import { headers } from "next/headers";
 
 export function GET(request: Request) {
   const geo = geolocation(request);
+  const header = headers().get("x-vercel-ip-city");
 
-  const headersList = headers();
-  const allHeaders: Record<string, string> = {};
-
-  headersList.forEach((value, key) => {
-    allHeaders[key] = value;
-  });
-
-  console.log("Location info", geo);
-  console.log("All headers", allHeaders);
-
-  return NextResponse.json({ geo, allHeaders });
+  return NextResponse.json({ ...geo, "x-vercel-ip-city": header });
 }
